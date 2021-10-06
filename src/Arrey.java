11465046d177fef28733557424da8b794a7d98a0
HP123-1 <hinalpandya79@gmail.com>
2021-10-06 19:16:16 +0100

    this is practice work

---
+++ src/Arrey.java
@@ -0,0 +1,13 @@
+public class Arrey {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+    }
+}
+
+
+
+
+
+
+
